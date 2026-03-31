f73655ee8201adc53c2f31705381b462e7ee60c1
Shobola <shorustamovismoilbek2@gmail.com>
2026-03-30 20:26:50 -0400

    Create RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Venus Store",
+  description: "Venus Store web app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="uz">
+      <body>{children}</body>
+    </html>
+  );
+}
